fd8329f95e2df5103b5568b6a6d947d8a469924f
mcankizilkaya <mcankizilkaya@gmail.com>
2024-11-27 12:30:39 +0000

    Removing ignored files

--- build/tbot3_tutorials/CMakeFiles/tbot3_tutorials_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/tbot3_tutorials_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/tbot3_tutorials_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/tbot3_tutorials/CMakeFiles/tbot3_tutorials_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for tbot3_tutorials_uninstall.
-# This may be replaced when dependencies are built.
